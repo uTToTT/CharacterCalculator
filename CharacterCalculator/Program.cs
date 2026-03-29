c3af499db2759a6986bb8cf7b678a7ef5343dcb4
Denys Zhukov <mr.pap2000@gmail.com>
2026-03-29 23:58:25 +0300

    Initial commit

---
+++ CharacterCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CharacterCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
